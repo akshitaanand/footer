98b99c424f2ef7c9acfdea4f5143d894c2d0b511
akshitaanand <63819293+akshitaanand@users.noreply.github.com>
2020-07-27 18:07:03 -0500

    Update App.js

--- App.js
+++ App.js
@@ -2,16 +2,17 @@ import React from "react";
 import "./App.css";
 import Header from "./Header";
 import Footer from "./Footer";
+import Terms from "./terms";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 export default function App() {
   return (
     <div className="main-container">
-      <header>
+      <Router>
         <Header />
-      </header>
-<footer>
-      <Footer />
-</footer>
+        <Route  />
+        <Footer />
+      </Router>
     </div>
   );
 }
